8ff7e669cfc440f99484e8ed113c301c9784c709
Nikita Lagoda <nikita.lagoda@gmail.com>
2024-02-10 16:11:36 +0300

    create payment + get status added

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import logging
+import config
+import requests
 from aiogram import executor
 from app import dp
 from handlers import choosing_shop_handlers, adding_product_handlers, cart_handlers
@@ -11,4 +13,11 @@ cart_handlers.register_handlers_cart(dp)
 
 
 if __name__ == "__main__":
+    payload = {}
+    headers = {
+        'Authorization': f'Bearer {config.JWT_TOKEN}'
+    }
+    url = f"https://enter.tochka.com/uapi/sbp/v1.0/customer/{config.CUSTUMER_CODE}/{config.BANK_CODE}"
+    response = requests.request("GET", url, headers=headers, data=payload)
+    print(response.text)
     executor.start_polling(dp, skip_updates=True)
--- payment.py
+++ payment.py
@@ -3,11 +3,45 @@ import json
 import config
 
 
+def create_payment(amount):
+    code = 404
+    while code != 200:
+        #url = f"https://enter.tochka.com/uapi/sbp/{config.API_VERSION}/qr-code/merchant/{config.MERCHANT_ID}/{config.ACCOUNT_ID}"
+        url = f'https://enter.tochka.com/sandbox/v2/sbp/{config.API_VERSION}/qr-code/merchant/{config.MERCHANT_ID}/{config.ACCOUNT_ID}'
+        payload = {
+            "Data": {
+                "amount": amount * 100,
+                "currency": "RUB",
+                "paymentPurpose": "Оплата за услуги",
+                "qrcType": "02",
+                "ttl": 10
+            }
+        }
+        headers = {
+            'Authorization': f'Bearer {config.JWT_SANDBOX}'
+        }
+        response = requests.request("POST", url, headers=headers, json=payload)
+        code = response.status_code
+    info = json.loads(response.text)
+    return (info['Data']['qrcId'], info['Data']['payload'])
+    
+
+def get_status(qr_id):
+    code = 404
+    while code != 200:
+        #url = f"https://enter.tochka.com/uapi/sbp/{config.API_VERSION}/qr-codes/{qr_id}/payment-status"
+        url = f'https://enter.tochka.com/sandbox/v2/sbp/{config.API_VERSION}/qr-codes/{qr_id}/payment-status'
+        payload = {}
+        headers = {
+            'Authorization': f'Bearer {config.JWT_SANDBOX}'
+        }
+        response = requests.request("GET", url, headers=headers, data=payload)
+        code = response.status_code
+    print(response.text)    
+
+    
+
 if __name__ == '__main__':
-    payload = {}
-    headers = {
-    'Authorization': f'Bearer {config.JWT_TOKEN}'
-    }
-    url = f"https://enter.tochka.com/uapi/sbp/v1.0/customer/{config.CUSTUMER_CODE}/{config.BANK_CODE}"
-    response = requests.request("GET", url, headers=headers, data=payload)
-    print(response.text)
+    pass
+    
+    
